bd40020494ab67f1113cb4dd6e2d4d5bbf340d97
Cesar Morales <cesarlenin2@live.com>
2020-05-07 09:55:14 -0700

    started migration

--- migrations/001.do.create-bookmarks.sql
+++ migrations/001.do.create-bookmarks.sql
@@ -3,5 +3,5 @@ CREATE TABLE bookmarks_list (
     title TEXT NOT NULL,
     url TEXT NOT NULL,
     description TEXT,
-    rating decimal(2,1)  NOT NULL
+    rating INTEGER  NOT NULL
 );
---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,7 @@
+INSERT INTO bookmarks_list(id,title,url,description,rating)
+
+VALUES
+('thinkful', 'https://overview.thinkful.com/','course', 3),
+('youtube', 'https://www.youtube.com/','video', 4),
+('humblebundle', 'https://www.humblebundle.com/','games', 1),
+('github', 'https://github.com/','course', 2)
